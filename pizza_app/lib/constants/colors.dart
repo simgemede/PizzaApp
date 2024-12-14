2f7c2d1d8c750f1ec458132951e55d90a86ff5cf
Simge Mede <109466550+simgemede@users.noreply.github.com>
2024-12-14 17:28:26 +0300

    .
    flutter + fonts + colors

---
+++ pizza_app/lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color turuncu1 = Color(0xFFF36F21);
+  static const Color turuncu2 = Color(0xFFFE5B00);
+  static const Color beyazimsigri = Color(0xFFF8F8F8);
+  static const Color siyah = Color(0xFF151515);
+  static const Color beyaz = Color(0xFFFFFFFF);
+}
